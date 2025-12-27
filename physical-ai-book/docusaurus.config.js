46bb2a63f09e2bf620fa521071f7fc7bdd954fe8
Rao Asif <163345273+RAO-ASIF@users.noreply.github.com>
2025-12-28 00:45:32 +0500

    Update production URL in docusaurus.config.js
    Paste Vercel Deployment URL

--- physical-ai-book/docusaurus.config.js
+++ physical-ai-book/docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://rao-asif.github.io',
+  url: 'https://vercel.com/asif-raos-projects/physical-ai-and-humanoid-robotics-book-ygt8',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
